41091f5e2a77351b4730168e108ae5e121666587
laudejulian-mc <laudejulianmari@gmail.com>
2025-08-13 08:00:10 +0800

    Add Part 1: Future with await

--- lib/part1_future.dart
+++ lib/part1_future.dart
@@ -1,1 +1,9 @@
 
+Future<String> delayedMessage() async {
+  return await Future.delayed(Duration(seconds: 2), () => 'Hello after 2 seconds!');
+}
+
+Future<void> main() async {
+  String message = await delayedMessage();
+  print(message);
+}
